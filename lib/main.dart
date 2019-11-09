e537baf7c9f552c69717f85656f145800b85e7b4
MadhaviG97 <47152272+MadhaviG97@users.noreply.github.com>
2019-11-09 09:10:25 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 // import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
-import './screens/homeScreen.dart';
+// import './screens/homeScreen.dart';
 import './screens/loginScreen.dart';
 
 void main() => runApp(MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'TrainTRACK',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
       home: Login(),
     );
