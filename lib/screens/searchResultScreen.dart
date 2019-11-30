e94b35db0ab4321489304603be11d3326bda0554
MadhaviG97 <47152272+MadhaviG97@users.noreply.github.com>
2019-11-30 09:46:02 +0530

    Update searchResultScreen.dart

---
+++ lib/screens/searchResultScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Imports extends StatefulWidget {  
+  String name;
+  String time;
+
+  Imports(String name, String time){
+    this.name=name;
+    this.time=time;
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    return null;
+  }
+}
